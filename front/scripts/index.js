b9a62cfcdd221d6628e8e54e3716d81c36d4b8df
YuberKill <yuberminecraft@gmail.com>
2024-03-06 03:42:09 -0400

    Tarea 1 (En Desarrollo)

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,27 @@
-console.log(tempData);
+
+
+
+let sliderInner = document.querySelector(".slider--inner");
+let image = sliderInner.querySelectorAll("img");
+let index = 0;
+
+function adjustImageSize() {
+    let sliderWidth = sliderInner.clientWidth; // Obtener el ancho del contenedor del slider
+    images.forEach(image => {
+        image.style.width = sliderWidth + "px"; // Establecer el ancho de la imagen igual al ancho del contenedor
+        image.style.height = "auto"; // Restablecer la altura automáticamente para mantener la proporción
+    });
+}
+
+
+let interval = setInterval(function(){
+    let percentage = index * -100;
+    
+    sliderInner.style.transform = "translateX(" + percentage + "%)";
+    index++;
+    if(index >= image.length - 1){
+        index = 0;
+    }
+
+}, 5000);
+
